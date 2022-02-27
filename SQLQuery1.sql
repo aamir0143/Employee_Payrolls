62cc4a391e6a00b4928d973fa4a17004ba3389f3
aamir0143 <mdaamirreza00@gmail.com>
2022-02-27 21:07:43 +0530

    [Aamir] Add : Updated Gender

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -10,11 +10,19 @@ StartDate date
 );
 ----------UC3 Create-add(C)-------------
 INSERT INTO Employee_Payroll (Name, Salary, StartDate) VALUES ('Salman', 25677.77, CAST('2000-01-01' as Date));
+INSERT INTO Employee_Payroll (Name, Salary, StartDate) VALUES ('Rehan', 25688.88, CAST('2001-02-02' as Date));
+INSERT INTO Employee_Payroll (Name, Salary, StartDate) VALUES ('Kajol', 284965.55, CAST('2008-05-07' as Date));
 INSERT INTO Employee_Payroll (Name, StartDate) VALUES ('Dhoni', getdate());
 ----------UC4 Select-Read(R)------------
 select * from Employee_Payroll;
 ----------UC5 Retrieve Salary Data For A Particular Employee----------
 select salary from employee_payroll where Name= 'Salman';
-select * from employee_payroll;
 select salary,Name from employee_payroll where startdate between CAST('2020-01-01' as Date) and getdate();
+----------UC6 Add Gender To Employee Payroll Table And Updated Row--------
+
+ALTER TABLE Employee_Payroll ADD Gender varchar(2);
+
+update employee_payroll set Gender = 'M' WHERE Id IN (1,2,7,8);
+update employee_payroll set Gender = 'F' WHERE Id IN (9);
+
 
