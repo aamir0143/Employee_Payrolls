4e812de344b64ad355cbd3fb862aed8c61c5c4b4
aamir0143 <mdaamirreza00@gmail.com>
2022-02-27 16:35:43 +0530

    [Aamir] Add : Create a Employee Payroll Table in the Payroll Service Database

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,3 +1,10 @@
 ----------UC1 Create Database----------
 create database Payroll_Service;
 use Payroll_Service;
+----------UC2 Create Table-------------
+CREATE TABLE Employee_Payroll(
+Id int identity(1,1) primary key,
+Name varchar(200),
+Salary float,
+StartDate date
+);
